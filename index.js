663a43e8635f6ea3c6d0d0afe367be9a0b1b390b
thekiwidev <adedotungabriel20@gmail.com>
2022-07-26 19:36:05 +0100

    scratched everything i did before to start aafresh, mobile-first this time, done with the header, Nav and showcase section on mobile screen 👌😊, to continue with medium screen sizes later 👋

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hello");
+// =============================================
+// DYNAMIC LOGO
+// =============================================
+var logoImgTag = document.querySelector(".logo img");
+var logoIcon = "./assets/icons/logo-icon-black-rounded-corners.svg";
+var logoFull = "./assets/icons/logo-full-black-rounded-corners.svg";
+window.addEventListener("load", function () {
+    if (window.innerWidth >= 1225) {
+        logoImgTag.src = logoFull;
+    }
+    else {
+        logoImgTag.src = logoIcon;
+    }
+});
+window.addEventListener("resize", function () {
+    if (window.innerWidth >= 1225) {
+        logoImgTag.src = logoFull;
+    }
+    else {
+        logoImgTag.src = logoIcon;
+    }
+});
+// =============================================
+// MENU TOGGLE
+// =============================================
+var menu = document.querySelector(".menu");
+var nav = document.querySelector("nav");
+menu.addEventListener("click", function () {
+    nav.classList.toggle("show");
+});
--- index.ts
+++ index.ts
@@ -1,1 +1,34 @@
-console.log(`Hello`);
+// =============================================
+// DYNAMIC LOGO
+// =============================================
+
+const logoImgTag: HTMLImageElement = document.querySelector(".logo img")!;
+const logoIcon = "./assets/icons/logo-icon-black-rounded-corners.svg";
+const logoFull = "./assets/icons/logo-full-black-rounded-corners.svg";
+
+window.addEventListener("load", () => {
+  if (window.innerWidth >= 1225) {
+    logoImgTag.src = logoFull;
+  } else {
+    logoImgTag.src = logoIcon;
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 1225) {
+    logoImgTag.src = logoFull;
+  } else {
+    logoImgTag.src = logoIcon;
+  }
+});
+
+// =============================================
+// MENU TOGGLE
+// =============================================
+
+const menu: HTMLDivElement = document.querySelector(".menu")!;
+const nav: HTMLElement = document.querySelector("nav")!;
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
